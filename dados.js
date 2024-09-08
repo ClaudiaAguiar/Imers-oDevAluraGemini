c63ffcb8330e578654140238a340f019d585e481
ClaudiaAguiar <claudia.aguiar.portela@gmail.com>
2024-09-07 21:35:49 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descrição: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descrição: "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas em competições internacionais, incluindo uma medalha de ouro olímpica.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "Ginastica artística  ouro"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descrição: "Beatriz Souza é uma judoca, sargento do exército e campeã olímpica brasileiraBeatriz Souza é uma atleta brasileira de [modalidade] que se destaca por [qualidade].",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    }
+]
+
